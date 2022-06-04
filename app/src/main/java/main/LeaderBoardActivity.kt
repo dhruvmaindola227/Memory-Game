f4ef7fb21b591778ced7a1be2db03992c50e9d84
dhruvmaindola227 <maindola.dhruv@gmail.com>
2022-06-04 17:18:11 +0530

    added sharedPreferences logic. Name is appearing null

--- app/src/main/java/main/LeaderBoardActivity.kt
+++ app/src/main/java/main/LeaderBoardActivity.kt
@@ -1,5 +1,7 @@
 package main
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONObject
 import com.example.gameattempt.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.json.JSONException
 import java.io.FileWriter
 import java.io.PrintWriter
@@ -16,6 +20,7 @@ import java.nio.charset.Charset
 import java.nio.file.Files.write
 
 class LeaderBoardActivity : AppCompatActivity() {
+    private var LIST_KEY = "list_key"
     lateinit var tvNameLabel: TextView
     lateinit var tvScoreLabel: TextView
     lateinit var tvDifficultyLabel: TextView
@@ -41,65 +46,9 @@ class LeaderBoardActivity : AppCompatActivity() {
             }
             else -> "hard"
         }
-        var highScoreList = ArrayList<Leaderboard>()
-        //Reading data from the json file.
-        try {
-            val obj = JSONObject(
-                Utils.getJsonDataFromAsset(
-                    applicationContext,
-                    "UserScoreData.json"
-                )!!
-            )
-            val scoreArray = obj.getJSONArray("scores")
 
-            for (i in 0 until scoreArray.length()) {
-                val user = scoreArray.getJSONObject(i)
-                val name = user.getString("name")
-                val score = user.getString("score")
-                val level = user.getString("level")
-                var leaderboard = Leaderboard(name, score, level)
-                highScoreList.add(leaderboard)
-            }
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        //before we write to the json we need to check for the highest score.
-//        for (i in 0 until highScoreList.size) {
-//            if (highScoreList[i].name == name) {
-//                if (highScoreList[i].level == level) {
-//                    if (highScoreList[i].highScore.toInt() < score) {
-//                        highScoreList[i].highScore = score.toString()
-//                    }
-//                } else {
-//                    highScoreList.add(Leaderboard(name, score.toString(), level))
-//                }
-//            }
-//        }
-//        highScoreList.add(Leaderboard(name!!, score.toString(), level))
-//        val json = JSONObject()
-//        try {
-//            val obj = JSONObject(
-//                Utils.getJsonDataFromAsset(
-//                    applicationContext,
-//                    "UserScoreData.json"
-//                )!!
-//            )
-//            val scoreArray = obj.getJSONArray("scores")
-//
-//            scoreArray.put(json.put("name" , name))
-//            scoreArray.put(json.put("score" , score))
-//            scoreArray.put(json.put("level" , level))
-////            for (i in 0 until scoreArray.length()) {
-////                val user = scoreArray.getJSONObject(i)
-////                user.put("name" , name)
-////                user.put("score" , score)
-////                user.put("level" , level)
-////            }
-//        } catch (e: JSONException) {
-//            e.printStackTrace()
-//        }
-
-        val adapter = ScoreAdapter(highScoreList)
+        val list = readListFromPreferences()
+        val adapter = ScoreAdapter(list)
         recyclerView.adapter = adapter
         btnBack.setOnClickListener {
             val intent = Intent(this@LeaderBoardActivity, MainActivity::class.java)
@@ -107,5 +56,14 @@ class LeaderBoardActivity : AppCompatActivity() {
         }
 
     }
+
+    fun readListFromPreferences(): ArrayList<Leaderboard> {
+        var sharePref = getSharedPreferences("userScores", Context.MODE_PRIVATE)
+        var jsonString = sharePref.getString(LIST_KEY, "")
+        var gson = Gson()
+        val type = object : TypeToken<List<Leaderboard>>() {}.type
+        return gson.fromJson(jsonString, type)
+    }
+
 
 }
--- app/src/main/java/main/SelectWords.kt
+++ app/src/main/java/main/SelectWords.kt
@@ -1,5 +1,6 @@
 package main
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +10,23 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gameattempt.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
 class SelectWords : AppCompatActivity() {
+    private var LIST_KEY = "list_key"
     private var numSet = HashSet<Int>()
     private var selectedCount = 0
     private var correctCount = 0
-    lateinit var scoreTv : TextView
-    lateinit var selectedTv : TextView
+    lateinit var scoreTv: TextView
+    lateinit var selectedTv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_words)
@@ -63,93 +70,120 @@ class SelectWords : AppCompatActivity() {
             }
             else -> "hard"
         }
-        var arrayOfTextViews = arrayListOf<TextView>(tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv10,
-                                                    tv11,tv12,tv13,tv14,tv15,tv16,tv17,tv18,tv19,tv20,
-                                                    tv21,tv22,tv23,tv24)
+        var arrayOfTextViews = arrayListOf<TextView>(
+            tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9, tv10,
+            tv11, tv12, tv13, tv14, tv15, tv16, tv17, tv18, tv19, tv20,
+            tv21, tv22, tv23, tv24
+        )
 
 
 
-        when(leveldiff){
+        when (leveldiff) {
 
             1 -> {
-                for (i in arrayOfTextViews.indices){
-                        arrayOfTextViews[i].text = WordArrays.easyArray[rand(0,23)]
+                for (i in arrayOfTextViews.indices) {
+                    arrayOfTextViews[i].text = WordArrays.easyArray[rand(0, 23)]
                 }
-                setClickListeners(arrayOfTextViews , leveldiff)
+                setClickListeners(arrayOfTextViews, leveldiff)
             }
 
-            2 ->{
-                for (i in arrayOfTextViews.indices){
-                    arrayOfTextViews[i].text = WordArrays.mediumArray[rand(0,23)]
+            2 -> {
+                for (i in arrayOfTextViews.indices) {
+                    arrayOfTextViews[i].text = WordArrays.mediumArray[rand(0, 23)]
                 }
-                setClickListeners(arrayOfTextViews , leveldiff)
+                setClickListeners(arrayOfTextViews, leveldiff)
             }
 
             3 -> {
-                for (i in arrayOfTextViews.indices){
-                    arrayOfTextViews[i].text = WordArrays.hardArray[rand(0,23)]
+                for (i in arrayOfTextViews.indices) {
+                    arrayOfTextViews[i].text = WordArrays.hardArray[rand(0, 23)]
                 }
-                setClickListeners(arrayOfTextViews , leveldiff)
+                setClickListeners(arrayOfTextViews, leveldiff)
             }
 
         }
-
-        var sharePref = getSharedPreferences("userScores" , Context.MODE_PRIVATE)
-        val editor = sharePref.edit()
+        var newUser = true
+        var list = ArrayList<Leaderboard>()
+        writeListToPreferences(list)
         btnFinish.setOnClickListener {
-            if(selectedCount < 10){
-                Toast.makeText(this , "You need to select 10 words." , Toast.LENGTH_SHORT).show()
+            if (selectedCount < 10) {
+                Toast.makeText(this, "You need to select 10 words.", Toast.LENGTH_SHORT).show()
             } else {
-                val name = userName
-                val score = correctCount
-
+                var leaderboard = Leaderboard(userName.toString() , correctCount.toString() , level)
+                list = readListFromPreferences()
+                if(list.isEmpty()){
+                    list.add(leaderboard)
+                    writeListToPreferences(list)
+                //if list in spfs is empty then add new leaderboard element
+                }else{ //otherwise compare previous values and then add.
+                    for (i in 0 until list.size){
+                        newUser = true
+                        if(list[i].name == userName){
+                            if(list[i].level == level){
+                                if(list[i].highScore.toInt() < correctCount){
+                                    list[i].highScore = correctCount.toString()
+                                    newUser = false
+                                    break
+                                }
+                            }else{
+                                list.add(leaderboard)
+                                newUser = false
+                                break
+                            }
+                        }
+                    }
+                    if(newUser){
+                        list.add(leaderboard)
+                    }
+                }
+                writeListToPreferences(list)
                 val intent = Intent(this@SelectWords, LeaderBoardActivity::class.java)
-                intent.putExtra("level", leveldiff)
-                val intent2 = Intent(this@SelectWords, LeaderBoardActivity::class.java)
-                intent2.putExtra("score" , correctCount)
                 startActivity(intent)
-                finish()
             }
         }
-
-    }
+            }
 
     private fun setClickListeners(arrayOfTextViews: ArrayList<TextView>, level: Int) {
         var wasClicked = false
-          for (i in arrayOfTextViews.indices){
-              arrayOfTextViews[i].setOnClickListener {
-                  wasClicked = true
-                  if(selectedCount < 10 && correctCount < 11){
-                  selectedCount++
-                      selectedTv.text = "$selectedCount/10"
-                  if(checkAnswer(arrayOfTextViews[i].text.toString() , level)){
-                      correctCount++
-                      arrayOfTextViews[i].background = ContextCompat.getDrawable(this ,
-                          R.drawable.correct_border
-                      )
-                          //loading our custom made animations
-                          val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-                          //starting the animation
-                          arrayOfTextViews[i].startAnimation(animation)
-                      scoreTv.text = "$correctCount/10"
-                  }else{
-                      arrayOfTextViews[i].background = ContextCompat.getDrawable(this ,
-                          R.drawable.wrong_border
-                      )
-                      val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-                      //starting the animation
-                      arrayOfTextViews[i].startAnimation(animation)
-                  }
-              }
-                  arrayOfTextViews[i].isEnabled = false
-              }
-          }
+        for (i in arrayOfTextViews.indices) {
+            arrayOfTextViews[i].setOnClickListener {
+                wasClicked = true
+                if (selectedCount < 10 && correctCount < 11) {
+                    selectedCount++
+                    selectedTv.text = "$selectedCount/10"
+                    if (checkAnswer(arrayOfTextViews[i].text.toString(), level)) {
+                        correctCount++
+                        arrayOfTextViews[i].background = ContextCompat.getDrawable(
+                            this,
+                            R.drawable.correct_border
+                        )
+                        //loading our custom made animations
+                        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+                        //starting the animation
+                        arrayOfTextViews[i].startAnimation(animation)
+                        scoreTv.text = "$correctCount/10"
+                    } else {
+                        arrayOfTextViews[i].background = ContextCompat.getDrawable(
+                            this,
+                            R.drawable.wrong_border
+                        )
+                        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+                        //starting the animation
+                        arrayOfTextViews[i].startAnimation(animation)
+                    }
+                }
+                arrayOfTextViews[i].isEnabled = false
+            }
+        }
     }
 
+
     private fun checkAnswer(textViewString: String, level: Int): Boolean {
-        when(level){
-            1 -> return WordArrays.easyArray.copyOfRange(0, 10).contains(textViewString) //10 because the end parameter is exclusive
-            2 -> return WordArrays.mediumArray.copyOfRange(0, 10).contains(textViewString)  //and we want 0-9 index.
+        when (level) {
+            1 -> return WordArrays.easyArray.copyOfRange(0, 10)
+                .contains(textViewString) //10 because the end parameter is exclusive
+            2 -> return WordArrays.mediumArray.copyOfRange(0, 10)
+                .contains(textViewString)  //and we want 0-9 index.
             3 -> return WordArrays.hardArray.copyOfRange(0, 10).contains(textViewString)
         }
         return false
@@ -158,12 +192,32 @@ class SelectWords : AppCompatActivity() {
     //to set different elements in different textviews.
     fun rand(start: Int, end: Int): Int {
         var numberToReturn = (start..end).random()
-        if(!numSet.contains(numberToReturn)){
-                numSet.add(numberToReturn)
-        }else{
-            numberToReturn = rand(start,end)
+        if (!numSet.contains(numberToReturn)) {
+            numSet.add(numberToReturn)
+        } else {
+            numberToReturn = rand(start, end)
         }
         return numberToReturn
+    }
+
+
+    @SuppressLint("CommitPrefEdits")
+    fun writeListToPreferences(list: ArrayList<Leaderboard>) {
+        var gson = Gson()
+        var jsonString: String = gson.toJson(list)
+        var sharePref = getSharedPreferences("userScores", Context.MODE_PRIVATE)
+        val editor = sharePref.edit()
+        editor.putString(LIST_KEY, jsonString)
+        editor.apply()
+    }
+
+
+    fun readListFromPreferences(): ArrayList<Leaderboard> {
+        var sharePref = getSharedPreferences("userScores", Context.MODE_PRIVATE)
+        var jsonString = sharePref.getString(LIST_KEY, "")
+        var gson = Gson()
+        val type = object : TypeToken<List<Leaderboard>>() {}.type
+        return gson.fromJson(jsonString, type)
     }
 
 }
